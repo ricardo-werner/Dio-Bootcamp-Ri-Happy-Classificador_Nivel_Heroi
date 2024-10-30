65f0a7e154fe67db1346710e6840c6bf9eaaf09c
Ricardo Werner <wernerdive@gmail.com>
2024-10-30 15:56:34 -0300

    feat: add acoes iniciais de estrutura de decisao

---
+++ src/scripts/script.js
@@ -0,0 +1,38 @@
+function calcularNivel() {
+  // Variáveis
+  const nome = document.getElementById("nomeHeroi").value;
+  const xp = parseInt(document.getElementById("xpHeroi").value);
+  let nivel;
+
+  // Estruturas de Decisão utilizando switch/case para intervalos de XP
+  switch (true) {
+    case xp <= 1000:
+      nivel = "Ferro";
+      break;
+    case xp <= 2000:
+      nivel = "Bronze";
+      break;
+    case xp <= 5000:
+      nivel = "Prata";
+      break;
+    case xp <= 7000:
+      nivel = "Ouro";
+      break;
+    case xp <= 8000:
+      nivel = "Platina";
+      break;
+    case xp <= 9000:
+      nivel = "Ascendente";
+      break;
+    case xp <= 10000:
+      nivel = "Imortal";
+      break;
+    default:
+      nivel = "Radiante";
+      break;
+  }
+
+  // Exibindo o resultado
+  const resultado = document.getElementById("resultado");
+  resultado.textContent = `O Herói de nome ${nome} está no nível de ${nivel}`;
+}
